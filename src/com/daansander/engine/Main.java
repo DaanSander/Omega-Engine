2093e014a6ed18734a30e8f4e1e5d86573e7b8b5
Daan <Daan@eriknoel.nl>
2015-09-19 14:18:26 +0200

    Initial commit

---
+++ src/com/daansander/engine/Main.java
@@ -0,0 +1,11 @@
+package com.daansander.engine;
+
+/**
+ * Created by Daan on 17-9-2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/daansander/engine/graphics/Sprite.java
@@ -0,0 +1,9 @@
+package com.daansander.engine.graphics;
+
+/**
+ * Created by Daan on 17-9-2015.
+ */
+public abstract class Sprite {
+
+
+}
