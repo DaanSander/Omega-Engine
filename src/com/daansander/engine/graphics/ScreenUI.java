afecc4ee0f740b7702c8c7ddff5b034322e0e0aa
Daan <Daan@eriknoel.nl>
2015-09-21 21:00:38 +0200

    Added Components

---
+++ src/com/daansander/engine/graphics/ScreenUI.java
@@ -0,0 +1,12 @@
+package com.daansander.engine.graphics;
+
+import java.util.HashSet;
+
+/**
+ * Created by Daan on 21-9-2015.
+ */
+public abstract class ScreenUI {
+
+    protected HashSet<Component> components = new HashSet<>();
+
+}
---
+++ src/com/daansander/engine/input/InputHandler.java
@@ -0,0 +1,69 @@
+package com.daansander.engine.input;
+
+import com.daansander.engine.graphics.Component;
+
+import javax.swing.event.MouseInputListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+/**
+ * Created by Daan on 21-9-2015.
+ */
+public class InputHandler extends Component implements MouseInputListener, MouseMotionListener, KeyListener {
+
+    public InputHandler() {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
