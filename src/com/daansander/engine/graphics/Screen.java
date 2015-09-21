1d68bfe46401e4f5d1857dff29ae8dc0764c2a5f
Daan <Daan@eriknoel.nl>
2015-09-21 21:01:09 +0200

    Added Components

--- src/com/daansander/engine/graphics/Screen.java
+++
@@ -1,7 +0,0 @@
-package com.daansander.engine.graphics;
-
-/**
- * Created by Daan on 21-9-2015.
- */
-public class Screen {
-}
--- src/com/daansander/engine/graphics/Sprite.java
+++
@@ -1,9 +0,0 @@
-package com.daansander.engine.graphics;
-
-/**
- * Created by Daan on 17-9-2015.
- */
-public abstract class Sprite {
-
-
-}
